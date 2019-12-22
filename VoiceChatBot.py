af502d78e603d2d1f0a074dccf94b9eecf898739
rinathalla <43606531+rinathalla@users.noreply.github.com>
2019-12-22 22:45:06 +0530

    Update VoiceChatBot.py

--- VoiceChatBot.py
+++ VoiceChatBot.py
@@ -4,9 +4,11 @@ os.system('color 3f')
 import win32com.client as wincl
 speak = wincl.Dispatch("SAPI.SpVoice")
 
+speak.Speak('Hello...Please start conversation by typing in output box....!!!!!')
+
 Question1 = input()
-print('Bot : Hi,I am your Wellness Assistant how may I help you?')
-speak.Speak('Hi,I am your Wellness Assistant how may I help you?')
+print('Bot : Hi,I am your Personal Assistant how may I help you?')
+speak.Speak('Hi,I am your Personal Assistant how may I help you?')
 
 Question3 = input()
 print('Partly to mostly cloud 29c')
@@ -17,8 +19,6 @@ speak.Speak('May I Know your Name ?')
 Name = input() #save answer
 print('Im glad to meet you, ' + Name + '!!') #reply
 speak.Speak('Im glad to meet you, ' + Name + '...!!!')
-
-"""
 Question2 = input()
 
 print('The number of letters of your name is:')
@@ -41,30 +41,8 @@ print('Awesome!, my brothers name is also ' + Reply + '!!') #reply
 speak.Speak('Awesome!, my brothers name is also ' + Reply + '!!')
 
 #keep going the conversation
-print('What is your aunts name?') #ask
-speak.Speak('What is your aunts name?')
-Reply = input() #save answer
-print('Ah ha! I do not have any aunt whose name is ' + Reply) #reply
-speak.Speak('Ah ha! I do not have any aunt whose name is ' + Reply)
-
-#keep going the conversation
-print('And do you have an aunt named Jeniffer?') #ask
-speak.Speak('And do you have an aunt named Jeniffer?')
-Reply = input() #save answer
-print('Okay, I have 2 aunts whose name is Jeniffer') #reply
-speak.Speak('Okay, I have 2 aunts whose name is Jeniffer')
-
-#keep going the conversation
 print('By the way, are you enjoying this conversation?') #ask
 speak.Speak('By the way, are you enjoying this conversation?')
 Reply = input() #save answer
 print('Oh nice, me too. I needed to talk to someone, even if its just a human. Although the machines give me more game! Just kidding ' + Name + '!!') #reply
 speak.Speak('Oh nice, me too. I needed to talk to someone, even if its just a human. Although the machines give me more game! Just kidding ' + Name + '!!')
-
-
-#keep going the conversation
-print('Can you lend me your new car?') #ask
-speak.Speak('Can you lend me your new car?')
-Reply = input() #save answer
-print('Oh, well, tomorrow Ill pick it up early. Perfect, well talk tomorrow when I come back. Bye ' + Name) #reply
- """
