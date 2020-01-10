646d463241a75b44c6100a22784f9257383d2beb
rinathalla <43606531+rinathalla@users.noreply.github.com>
2020-01-11 00:11:33 +0530

    Update VoiceChatBot.py

--- VoiceChatBot/VoiceChatBot.py
+++ VoiceChatBot/VoiceChatBot.py
@@ -7,43 +7,43 @@ speak = wincl.Dispatch("SAPI.SpVoice")
 speak.Speak('Hello...Please start conversation by typing in output box....!!!!!')
 
 Question1 = input()
-print('Bot : Hi,I am your Personal Assistant how may I help you?')
+print('🤖 : Hi,How may I help you❓')
 speak.Speak('Hi,I am your Personal Assistant how may I help you?')
 
 Question3 = input()
-print('Partly to mostly cloud 29c')
-speak.Speak('Partly to mostly cloud 29c?')
+print('🤖 : Partly to mostly cloud 29 dc⛅️')
+speak.Speak('Partly to mostly cloud 29 degree celcius ?')
 
-print('Bot : May I Know your Name ?')
+Question4 = input()
+print('🤖 : May I Know your Name ❓')
 speak.Speak('May I Know your Name ?')
-Name = input() #save answer
-print('Im glad to meet you, ' + Name + '!!') #reply
-speak.Speak('Im glad to meet you, ' + Name + '...!!!')
+Name = input()
+
+print('🤖 : I am glad to meet you ,' + Name +' ' '😊')
+speak.Speak('I am glad to meet you, ' + Name )
 Question2 = input()
 
-print('The number of letters of your name is:')
+print('🤖 : The number of letters of your name is:')
 speak.Speak('The number of letters of your name is:')
 print(len(Name))
 speak.Speak(len(Name))
 
-#keep going the conversation
-print('How old are you?') #ask
+print('🤖 : How old are you 🤔 ?') #ask
 speak.Speak('How old are you?')
-Reply = input() #save answer
-print('Okay, then you will be ' + str(int(Reply) + 1) + ' next year.') #reply
+Reply = input()
+print('🤖 : Okay, then you will be ' + str(int(Reply) + 1) + ' next year 🎊 🎉')
 speak.Speak('Okay, then you will be ' + str(int(Reply) + 1) + ' next year.')
 
-#keep going the conversation
-print('Whats the name of your brother?') #ask
-speak.Speak('Whats the name of your brother?')
-Reply = input() #save answer
+print('🤖 : How was your day')
+speak.Speak('How was your day')
+Reply = input()
 
-print('Awesome!, my brothers name is also ' + Reply + '!!') #reply
-speak.Speak('Awesome!, my brothers name is also ' + Reply + '!!')
+print('🤖 : Glad to hear it....😄 ')
+speak.Speak('Glad to hear it')
 
-#keep going the conversation
-print('By the way, are you enjoying this conversation?') #ask
+
+print('🤖 : By the way, are you enjoying this conversation 🤔?')
 speak.Speak('By the way, are you enjoying this conversation?')
-Reply = input() #save answer
-print('Oh nice, me too. I needed to talk to someone, even if its just a human. Although the machines give me more game! Just kidding ' + Name + '!!') #reply
+Reply = input()
+print('🤖 : Oh nice, me too. I needed to talk to someone, even if its just a human''\n''Although the machines give me more game.''Just kidding ' + Name + '...😂😂😂')
 speak.Speak('Oh nice, me too. I needed to talk to someone, even if its just a human. Although the machines give me more game! Just kidding ' + Name + '!!')
